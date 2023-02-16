648f28ff627f91daa8f98268d3d003c0379ef5a3
Gor Stepanyan <gor.stepanyan027@gmail.com>
2023-02-16 20:34:39 +0100

    Add switch connection under heavy load parameters

--- scylla.go
+++ scylla.go
@@ -290,7 +290,7 @@ func (p *scyllaConnPicker) maybeReplaceWithLessBusyConnection(c *Conn) *Conn {
 		return c
 	}
 	alternative := p.leastBusyConn()
-	if alternative != nil && alternative.InUseStreams()*100 < c.InUseStreams()*80 {
+	if alternative != nil && alternative.InUseStreams()*100 < c.InUseStreams()*(100 - c.session.cfg.HeavyLoadedSwitchConnectionPercentage) {
 		return alternative
 	} else {
 		return c
@@ -298,7 +298,7 @@ func (p *scyllaConnPicker) maybeReplaceWithLessBusyConnection(c *Conn) *Conn {
 }
 
 func isHeavyLoaded(c *Conn) bool {
-	return c.streams.NumStreams/2 > c.AvailableStreams()
+	return c.InUseStreams() > c.session.cfg.HeavyLoadedConnectionThreshold
 }
 
 func (p *scyllaConnPicker) leastBusyConn() *Conn {
